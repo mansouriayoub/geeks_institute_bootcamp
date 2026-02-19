b653cf50dbac00221a40edead26cbad313ee2e2e
mansouriayoub <mansouriayoub@protonmail.com>
2026-02-19 10:18:29 +0000

    Daily Challenge : Promise.all() - Day 2 of week 3 Added sunrise time display for Paris and New York: Updated HTML to include sunrise time fields and modified JavaScript to fetch and display sunrise times using Promise.all for API calls.

--- week_3/day_2/daily_challenge/DailyChallenge_Promise.all/script.js
+++ week_3/day_2/daily_challenge/DailyChallenge_Promise.all/script.js
@@ -6,20 +6,31 @@ const lon1 = document.querySelector('#lon1')
 const lat2 = document.querySelector('#lat2')
 const lon2 = document.querySelector('#lon2')
 
-console.log(form);
-
-form.addEventListener('submit', (e)=>{
+const submitFunc = async (e)=>{
     e.preventDefault();
-
-    const city1 = `https://api.sunrise-sunset.org/json?lat=${lat1}&lng=${lon1}`
-    const city2 = `https://api.sunrise-sunset.org/json?lat=${lat2}&lng=${lon2}`;
-
-    const promises = [city1, city2];
-
-    // const result = Promise.all(promises).then();
-    // console.log(JSON.stringify(result));
-
-    console.log(Promise.all(JSON.parse(promises)));
     
-    
-})
+
+    const city1 = `https://api.sunrise-sunset.org/json?lat=${lat1.value}&lng=${lon1.value}`
+    const city2 = `https://api.sunrise-sunset.org/json?lat=${lat2.value}&lng=${lon2.value}`;
+
+    const promises = [
+        fetch(city1).then(res => res.json()), 
+        fetch(city2).then(res => res.json())
+    ];
+
+    try {
+        let result = await Promise.all(promises)
+        // console.log(result[0].results.sunrise)
+
+        const parisSunrise = document.querySelector('#parisSunrise')
+        const newYorkSunrise = document.querySelector('#newYorkSunrise')
+
+        parisSunrise.textContent = result[0].results.sunrise
+        newYorkSunrise.textContent = result[1].results.sunrise
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+form.addEventListener('submit', submitFunc)
