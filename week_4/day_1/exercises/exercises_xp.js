e8182411dae3b445bb24667bffd1da4fdda1abf2
mansouriayoub <mansouriayoub@protonmail.com>
2026-02-28 12:30:58 +0000

    Implemented MongoDB operations for student enrollment and data manipulation in exercises XP of week 4, day 1

--- week_4/day_1/exercises/exercises_xp.js
+++ week_4/day_1/exercises/exercises_xp.js
@@ -1,13 +1,33 @@
+// 🛠 Exercise 1: The Enrollment Phase
+use globalTechAcademy
+
+db.students.insertMany([
+    { name: "Yassine", age: 19, city: "Rabat", major: "AI" },
+    { name: "Sophia", age: 26, city: "Paris", major: "Cybersecurity" },
+    { name: "Kaito", age: 22, city: "Tokyo", major: "Web Dev" },
+    { name: "Elena", age: 31, city: "Madrid", major: "Data Science" },
+    { name: "Marcus", age: 24, city: "New York", major: "AI" }
+])
+
+
+// 🔍 Exercise 2: Data Intelligence
+// Task A
 db.students.find({ age: { $gte: 18, $lte: 25 } })
 
+// Task B
 db.students.updateOne({ name: "Yassine" }, { $set: { city: "Casablanca" } })
 
+// Task C
 db.students.deleteOne({ name: "Sophia" })
 
+// 🏆 Boss Level Challenge (Optional)
 db.students.updateMany({ major: "AI" }, { $set: { major: "Generative AI" } })
 
 
+
+// current state of db
 db.students.find().pretty()
+
 [
     {
         _id: ObjectId('69a2d857a4e3adfdd88563b1'),
