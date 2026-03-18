11e2c8d1704d867bcce4bed19fb112943b26e0d4
mansouriayoub <mansouriayoub@protonmail.com>
2026-03-18 19:46:27 +0000

    feat: add Garage component to Car component and initialize size variable

--- week_7/day_3/Exercises_XP/src/Components/Car.jsx
+++ week_7/day_3/Exercises_XP/src/Components/Car.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import Garage from "./Garage";
 
 const Car =({model})=>{
     const [color, setColor] = useState('Blue')
+    let size = 'small'
     return(
+        <>
         <header>This car is <span style={{color:color}}>{color}</span> {model}.</header>
+        <Garage size />
+        </>
     )
 }
 export default Car;
