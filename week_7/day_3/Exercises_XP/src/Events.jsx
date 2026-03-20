af001f7a71eb05aeae348335e69f694cb9772d9c
mansouriayoub <mansouriayoub@protonmail.com>
2026-03-20 23:14:03 +0000

    feat: implement input handling in Events component with Enter key detection and toggle button

--- week_7/day_3/Exercises_XP/src/Events.jsx
+++ week_7/day_3/Exercises_XP/src/Events.jsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
+
 const Events = ()=>{
     const clickMe = ()=>{
         alert('I was clicked');
     }
 
-    /*In the Events Functional component, in the return, 
-    create an input tag that has an onKeyDown event handler, 
-    that listens to a function called handleKeyDown.*/
-    const handleKeyDown=()=>{
+    const handleKeyDown=(e)=>{
+        if (e.key === 'Enter') {   
+            alert(`The Enter key was pressed, your input is: ${e.target.value}`)
+        }
+    }
 
+    const [isToggleOn, setIsToggleOn] = useState(true)
+
+    const handleToggleOn =()=>{
+        setIsToggleOn(prev=>!prev)
     }
 
     return (
         <>
             <button onClick={clickMe}>Click me</button>
             <input type="text" onKeyDown={handleKeyDown} />
+            <button onClick={handleToggleOn}>{isToggleOn ? 'ON' : 'OFF'}</button>
         </>
     )
 }
