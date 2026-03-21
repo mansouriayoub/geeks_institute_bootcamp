0288762056336b74174cb73de2dce97db3d98f02
mansouriayoub <mansouriayoub@protonmail.com>
2026-03-21 19:17:43 +0000

    feat: refactor Forms component to manage user state as an object and add age input with validation

--- week_7/day_3/Exercises_XP_Gold/src/Components/Forms.jsx
+++ week_7/day_3/Exercises_XP_Gold/src/Components/Forms.jsx
@@ -1,25 +1,41 @@
 import { useState } from "react"
 
 const Forms =()=>{
-    const [username, setUsername] = useState('')
+    const [user, setUser] = useState({
+        name: '',
+        age: null,
+        errormessage: ''
+    })
 
-    const handleName =(e)=>{
-        setUsername(e.target.value)
+    const handleChange = (e) => {
+        // since e.target only has .name and .value, not .age. We only need the correct destructure:
+        const { name, value } = e.target;
+        setUser(prevUser => ({
+            ...prevUser,
+            [name]: value,
+            errormessage: name === 'age' && value && isNaN(value) ? 'Your age must be a number' : ''
+        }));
+    
     }
 
     let header = '';
-    if(username) header = <h1>Hello {username} </h1>
+    if(user.name) header = <h1>Hello {user.name} {user.age} </h1>
 
     const mySubmitHandler =(e)=>{
         e.preventDefault()
-        alert(`You are submitting ${username}`)
+        alert(`You are submitting ${user.name}`)
     }
+
+    
 
     return (
         <form  onSubmit={mySubmitHandler}>
             {header}
             <label htmlFor="name">Enter your name</label>
-            <input type="text" name="name" id="name" onChange={handleName} />
+            <input type="text" name="name" id="name" onChange={handleChange} />
+            <label htmlFor="age">Enter your age</label>
+            <input type="text" name="age" id="age" onChange={handleChange} />
+            <span>{user.errormessage}</span>
             <input type="submit" value="submit" />
         </form>
     )
