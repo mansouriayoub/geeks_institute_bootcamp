848ad5ec7a8806cd24c5681492f797c360a56684
mansouriayoub <mansouriayoub@protonmail.com>
2026-03-21 18:20:42 +0000

    feat: add form submission handling to Forms component in Exercise XP Gold

--- week_7/day_3/Exercises_XP_Gold/src/Components/Forms.jsx
+++ week_7/day_3/Exercises_XP_Gold/src/Components/Forms.jsx
@@ -9,11 +9,18 @@ const Forms =()=>{
 
     let header = '';
     if(username) header = <h1>Hello {username} </h1>
+
+    const mySubmitHandler =(e)=>{
+        e.preventDefault()
+        alert(`You are submitting ${username}`)
+    }
+
     return (
-        <form>
+        <form  onSubmit={mySubmitHandler}>
             {header}
             <label htmlFor="name">Enter your name</label>
             <input type="text" name="name" id="name" onChange={handleName} />
+            <input type="submit" value="submit" />
         </form>
     )
 }
