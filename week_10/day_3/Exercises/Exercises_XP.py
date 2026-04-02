b9c0429615a07c87f3b73cea6aa9a195d85bf607
mansouriayoub <mansouriayoub@protonmail.com>
2026-04-02 19:51:52 +0100

    feat: add Zara exercise with dictionary manipulation tasks including updates, deletions, and prints

--- week_10/day_3/Exercises/Exercises_XP.py
+++ week_10/day_3/Exercises/Exercises_XP.py
@@ -48,3 +48,57 @@ for name, age in familyDict.items():
 		total += 15
 		
 print(f"The family's total cost for the movies is {total}")
+
+#🌟 Exercise 3: Zara
+"""
+1. Create a dictionary called brand which value is the information from part one (turn the info into keys and values).
+The values type_of_clothes and international_competitors should be a list. The value of major_color should be a dictionary.
+"""
+brand = {
+	"name": "Zara", 
+	"creation_date": 1975 ,
+	"creator_name": "Amancio Ortega Gaona", 
+	"type_of_clothes": ["men", "women", "children", "home"], 
+	"international_competitors": ["Gap", "H&M", "Benetton"],
+	"number_stores": 7000, 
+	"major_color": {
+		"France": "blue", 
+		"Spain": "red", 
+		"US": ["pink", "green"],
+	}
+}
+#2. Change the number of stores to 2.
+brand["number_stores"]=2
+
+#3. Use the key [type_of_clothes] to print a sentence that explains who Zaras clients are.
+print(f"Zara's types of clients are {range(brand["types__clothes"])}.")
+#4. Add a key called country_creation with a value of Spain.
+brand["country_creation"]="Spain"
+#5. Check if the key international_competitors is in the dictionary. If it is, add the store Desigual.
+if brand["international_competitors"] in brand:
+	brand["international_competitors"] = "Desigual"
+
+#6. Delete the information about the date of creation.
+del brand["creation_date"]
+#7. Print the last international competitor.
+print(brand["international_competitors"][-1])
+#8. Print the major clothes colors in the US.
+print(brand["major_color"]["US"])
+#9. Print the amount of key value pairs (ie. length of the dictionary).
+print(len(brand))
+# 10. Print the keys of the dictionary.
+print(brand.keys())
+"""
+11. Create another dictionary called more_on_zara with the following details:
+- creation_date: 1975
+- number_stores: 10 000
+"""
+more_on_zara = {
+	"creation_data": 1975,
+	"number_stores": 10000
+}
+#12. Use a method to add the information from the dictionary more_on_zara to the dictionary brand.
+brand.update(more_on_zara)
+#13. Print the value of the key number_stores. What just happened ?
+print(brand["number_stores"])
+# The value of 'number_stores' is now 10000 because the update() method overwritten the previous value in brand with the value from more_on_zara.
