05ae9d5d370d48ee3baf84bfb64f191f16807c5f
mansouriayoub <mansouriayoub@protonmail.com>
2026-04-02 19:26:04 +0100

    feat: update Cinemax exercise to allow user input for family members and their ages, replacing hardcoded values

--- week_10/day_3/Exercises/Exercises_XP.py
+++ week_10/day_3/Exercises/Exercises_XP.py
@@ -6,24 +6,7 @@ values = [10, 20, 30]
 for item in zip(keys,values):
 	print(item)
 
-"""
-🌟 Exercise 2 : Cinemax #2
-
-A movie theater charges different ticket prices depending on a person’s age.
-
-if a person is under the age of 3, the ticket is free.
-if they are between 3 and 12, the ticket is $10.
-
-if they are over the age of 12, the ticket is $15.
-Given the following object:
-
-How much does each family member have to pay ?
-
-Print out the family’s total cost for the movies.
-
-Bonus: Ask the user to input the names and ages instead of using the provided family variable 
-(Hint: ask the user for names and ages and add them into a family dictionary that is initially empty).
-"""
+#🌟 Exercise 2 : Cinemax #2
 family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
 
 total = 0
@@ -32,19 +15,36 @@ for name, age in family.items():
 	if age < 3:
 		print(f"The ticket is free for {name}")
 	elif age >= 3 and age <= 12:
-		"""
-		Or more Pythonically, this can be written as a chained comparison:
-		3 <= age <= 12
-		This is one of Python's elegant features — it reads exactly like math notation.
-		"""
 		print(f"The ticket is $10 for {name}")
 		total += 10
 	else:
 		print(f"The ticket is $15 for {name}")
 		total += 15
 		
-print(f"The family’s total cost for the movies is {total}")
+print(f"The family's total cost for the movies is {total}")
 
-# userInput = input(f"put your name and age")
+"""
+Bonus: Ask the user to input the names and ages instead of using the provided family variable 
+(Hint: ask the user for names and ages and add them into a family dictionary that is initially empty).
+"""
+familyDict = {}
+userInput = int(input("How many familly members"))
 
-# familyDict = {userInput}
+for member in range(userInput):
+	name = input("What is the name of this member")
+	age = int(input("What is the age of this member"))
+	familyDict[name] = age
+
+total = 0
+
+for name, age in familyDict.items():
+	if age < 3:
+		print(f"The ticket is free for {name}")
+	elif 3 <= age <= 12: #chained comparison (This is one of Python's elegant features — it reads exactly like math notation.)
+		print(f"The ticket is $10 for {name}")
+		total += 10
+	else:
+		print(f"The ticket is $15 for {name}")
+		total += 15
+		
+print(f"The family's total cost for the movies is {total}")
